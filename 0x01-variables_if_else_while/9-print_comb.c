6d9eb15d8fb341638da42690323d8d0972447ce6
1e-gilbert <danielleagboka@gmail.com>
2023-06-16 17:35:05 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,13 @@ int main(void)
 int n;
 
 for (n = 48; n <= 57; n++)
-{
 putchar(n);
+
+if (n != 57) // check if it's not the last number (57)
+{
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
